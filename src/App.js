54850bf5e7842dac86be04bbc9cdf05bec586be9
Andrew Ong <andrew.ong@global.ntt>
2026-02-09 16:04:30 +0800

    Add counter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { useState } from "react";
+
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div>
-      <h1>Hello React 👋</h1>
-      <p>My app is running.</p>
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Add</button>
     </div>
   );
 }
