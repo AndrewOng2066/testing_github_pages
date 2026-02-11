c4f90fa04f9ad9416995989b4ab1550789c84329
Andrew Ong <andrew.ong@global.ntt>
2026-02-11 10:03:18 +0800

    Create second page

---
+++ src/pages/Login.js
@@ -0,0 +1,8 @@
+export default function Login() {
+    return (
+        <div style={{ padding: 24 }}>
+            <h2>Login Page</h2>
+            <p>Please Log in...</p>
+        </div>
+    );
+}
